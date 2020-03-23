b769da05ff84bfa46af46b455a0e87aeb7df448a
aman5319 <amanpandey5319@gmail.com>
2020-03-23 17:23:41 +0530

    precision recall f1 score added

--- report.py
+++ report.py
@@ -5,19 +5,19 @@ import torch
 import copy
 from utils import convert_prob_to_label
 import seaborn as sns
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, classification_report
 import matplotlib.pyplot as plt
 
 
 class Report:
-    def __init__(self, writer=None, train_type=None, classes=None):
+    def __init__(self, writer=None, classes=None):
         self.writer = SummaryWriter() if writer is None else writer
         self.counter = 0
-        self.train_type = ["train", "valid", "test"] if train_type is None else train_type
+        self.train_type = ["train", "valid"]
         self.classes = classes
         self.clean_flag = True
 
-    def write_a_batch(self, loss, batch_size, actual=None, prediction=None, train_type="train"):
+    def write_a_batch(self, loss, batch_size, actual, prediction, train_type="train"):
         if self.clean_flag:
             self.init_data_storage()
         assert train_type in self.train_type, f"Train type {train_type} not in the list {self.train_type}"
@@ -27,8 +27,6 @@ class Report:
         return self
 
     def update_actual_prediction(self, actual, prediction, train_type):
-        if actual is None and prediction is None:
-            return self
         actual = self.change_data_type(actual, "np")
         pred = self.change_data_type(prediction, "np")
         if "actual" not in self.act_pred_dict[train_type]:
@@ -103,11 +101,28 @@ class Report:
                             c = cm[i, j]
                             p = cm_perc[i, j]
                             annot[i, j] = "0" if c == 0 else f"{c}/{summ}\n{p:.1f}%"
-                fig = plt.figure(figsize = (10, 10) if simple else (15,10))
-                ax = sns.heatmap(cm, annot=True if simple else annot,
+                fig = plt.figure(figsize=(10, 10) if simple else (15, 10))
+                sns.heatmap(cm, annot=True if simple else annot,
                                  fmt="d" if simple else "",
                                  linewidth=.5, cmap="YlGnBu", linecolor="Black",
                                  figure=fig,
                                  xticklabels=self.classes, yticklabels=self.classes)
                 self.writer.add_figure(f"Confusion Matrix/{tag}", fig, global_step=self.counter)
+        return self
+
+    def plot_precision_recall(self):
+        if all(["train" in self.train_type, "valid" in self.train_type]):
+            actual, pred = self.act_pred_dict["valid"]["actual"], convert_prob_to_label(self.act_pred_dict["valid"]["pred"])
+            output_valid = classification_report(actual, pred, output_dict=True)
+            actual, pred = self.act_pred_dict["train"]["actual"], convert_prob_to_label(self.act_pred_dict["train"]["pred"])
+            output_train = classification_report(actual, pred, output_dict=True)
+            for key in output_train:
+                if isinstance(output_train[key], dict):
+                    for key1 in output_train[key]:
+                        if key1 != "support":
+                            scaler_tag = {"train": output_train[key][key1], "valid": output_valid[key][key1]}
+                            self.writer.add_scalars(f"{key}/{key1}", scaler_tag, self.counter)
+                else:
+                    scaler_tag = {"train": output_train[key], "valid": output_valid[key]}
+                    self.writer.add_scalars(key, scaler_tag, self.counter)
         return self
